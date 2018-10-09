d49de9238dce8a4165a9688bc73d5a7b0f1e0d20
Eduardo <edumoralesjr1996@gmail.com>
2018-10-09 21:13:41 +0800

    123

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,8 +2,15 @@
 	class MY_Controller extends MX_Controller{
 		
 		function __construct(){
-			
+			parent::__construct();
 		}
-		
+
+		public function load_page($page,$data = array()){
+			$this->load->view('head',$data);
+			$this->load->view('nav',$data);
+			$this->load->view($page,$data);
+			$this->load->view('footer',$data);
+		}
+
 	}
 ?>
--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -1,3 +1,15 @@
 <?php
-	
+	/**
+	 * 
+	 */
+	class Login extends MY_Controller{
+		
+		function __construct(){
+			parent::__construct();
+		}
+
+		public function index(){
+			$this->load_page("index");
+		}
+	}
 ?>
